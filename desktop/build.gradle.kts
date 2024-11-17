d5e044783af62fcc57099e71f77b366e2a5601f2
TarasovVP <vptarasov1998@gmail.com>
2024-11-17 10:24:46 +0200

    Change modular structure

---
+++ desktop/build.gradle.kts
@@ -0,0 +1,52 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    jvm("desktop") {
+        tasks.withType<KotlinJvmCompile>().configureEach {
+            compilerOptions {
+                jvmTarget.set(JvmTarget.JVM_17)
+            }
+        }
+    }
+    sourceSets {
+        val desktopMain by getting
+
+        desktopMain.dependencies {
+            implementation(projects.shared)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.components.resources)
+            implementation(libs.androidx.viewmodel.compose)
+            //Koin
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose)
+            //Coil
+            implementation(libs.coil.compose)
+            implementation(libs.coil.network.ktor)
+            //Navigation
+            implementation(libs.navigation.compose)
+            implementation(libs.koin.core)
+            implementation(compose.desktop.currentOs)
+        }
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "com.vnteam.architecturetemplates"
+            packageVersion = "1.0.0"
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,10 +17,9 @@ dependencyResolutionManagement {
     }
 }
 
-include(":shared")
 include(":core")
-include(":composeUi")
-include(":composeApp")
-include(":composeWebApp")
+include(":shared")
+include(":mobile")
+include(":desktop")
+include(":web")
 include(":server")
-include(":webApp")
